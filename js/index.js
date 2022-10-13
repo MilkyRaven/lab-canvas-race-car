a0cfdcbe2ece288e4cf98eb02eca51b2a44383dd
Milky Raven <heartonllamas@gmail.com>
2022-10-13 15:44:35 +0200

    background and car

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+const background = new Image();
+background.src = "./images/road.png";
+const car = new Image();
+car.src = "./images/car.png";
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(background,0,0, canvas.width, canvas.height);
+    ctx.drawImage(car, (canvas.width / 2 - 25), (canvas.height -70), 50, 70);
+  }
 };
+
